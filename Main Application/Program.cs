1b257bcfa9c0271ca6c0ebaa3ebd8ddba0277a40
Dylan Laufenberg <dylan.laufenberg@gmail.com>
2020-03-28 11:33:31 -0700

    Removed last access check.
    Instead, for reliability, try to copy each time, but
    don't overwrite existing files. This prevents any
    weird edge cases involving old files that error out
    and are never re-checked and is a safer approach
    overall.
    
    Updated error handling to handle existing files.

--- Main Application/Program.cs
+++ Main Application/Program.cs
@@ -10,11 +10,6 @@ namespace Main_Application
 {
     class Program
     {
-        const string LAST_ACCESS_FILE = "last_access";
-        const string LAST_ACCESS_DATETIME_FORMAT = "yyyy-MM-ddTHH:mm:ssZ";
-        static readonly System.Globalization.CultureInfo LAST_ACCESS_CULTURE_INFO = 
-            System.Globalization.CultureInfo.InvariantCulture;
-
         // Constants that should eventually be specified by a settings file.
         const string destination_dir = "My Spotlight Backgrounds";
         const string landscape_dir = "Landscape";
@@ -37,60 +32,10 @@ namespace Main_Application
                 Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) +
                 @"\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets";
             
-            // Read the last access file, if it exists. This will either be null (if no last access value can be
-            // retrieved) or a DateTime we can use to compare with file access times.
-            DateTime? last_access = null;
-            try {
-                string last_access_string = File.ReadAllText(LAST_ACCESS_FILE, System.Text.Encoding.UTF8);
-                last_access =
-                    DateTime.ParseExact(
-                        last_access_string,
-                        LAST_ACCESS_DATETIME_FORMAT,
-                        LAST_ACCESS_CULTURE_INFO
-                    );
-            } catch(FileNotFoundException) {
-                // Since the file wasn't found, assume this is the first run.
-                // Do nothing; last_access is already null.
-            } catch(IOException e) {
-                // An I/O error occurred opening the file.
-                // Print it and continue; let last_access remain null.
-                Console.Error.WriteLine(e);
-            } catch(Exception e) {
-                // All other errors should crash the program.
-                Console.Error.WriteLine(e);
-                System.Environment.Exit(1);
-            }
-
-            // Update the last access file to the current time.
-            //
-            // Note: there's inherently a race condition where the environment could change during program execution.
-            // We record the current time as the last access time so that, if this happens, the very worst-case
-            // outcome is that we have a duplicate file. In no case will we miss a file due to this resolution of the
-            // race condition.
-            try {
-                using (FileStream file = File.OpenWrite(LAST_ACCESS_FILE)) {
-                    string datestring =
-                        DateTime.UtcNow.ToString(
-                            LAST_ACCESS_DATETIME_FORMAT,
-                            LAST_ACCESS_CULTURE_INFO
-                        );
-                    file.Write(new UTF8Encoding(false).GetBytes(datestring), 0, datestring.Length);
-                }
-            } catch(Exception e) {
-                // Report any exceptions here, but continue program execution, since we can clearly proceed.
-                Console.Error.WriteLine(e);
-            }
-
             // Process each file in the source directory.
             try {
                 foreach (string file in Directory.EnumerateFiles(source_dir)) {
                     string filename = Path.GetFileName(file);
-
-                    if (last_access.HasValue && File.GetLastAccessTime(file) < last_access.Value) {
-                        // Our last run was after this file was created, so we should skip it.
-                        Console.WriteLine($"Skipping old file: {filename}");
-                        continue;
-                    }
 
                     try {
                         using (FileStream stream = File.OpenRead(file)) {
@@ -128,9 +73,15 @@ namespace Main_Application
                         Console.WriteLine($"Skipping non-JPEG file: {filename}");
                         continue;
                     } catch (Exception e) {
-                        // Something went wrong with this file. Print an error and skip the file.
-                        Console.Error.WriteLine($"Error processing file: {filename}");
-                        Console.Error.WriteLine(e.Message);
+                        if (e.Message.IndexOf("exists") >= 0) {
+                            // The file exists, so we'll just skip it. This isn't really an error;
+                            // it's normal, expected behavior.
+                            Console.WriteLine($"File already exists: {filename}.jpg");
+                        } else {
+                            // Something went wrong with this file. Print an error and skip the file.
+                            Console.Error.WriteLine($"Error processing file: {filename}");
+                            Console.Error.WriteLine(e.Message);
+                        }
                         continue;
                     }
                 }
